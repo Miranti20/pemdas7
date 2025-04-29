b9095779a9a52e0c2b3fb49cf88dac42b095c6e1
Miranti20 <rantirarrr4505@gmail.com>
2025-04-29 11:08:25 +0700

    Perubahan print_data

--- Introduce1.cpp
+++ Introduce1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-Class mahasiswa {
+class mahasiswa {
     public:
     int nim;
     string nama;
     float nilai;
-}
+
+    void print_data(){
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+
+};
